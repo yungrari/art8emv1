90b0f4596fdceefe585a470530549bef563509bf
Young Emo <youngemo@Youngs-MacBook-Pro.local>
2024-11-27 16:43:22 +0100

    Remove @vercel/blob

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,42 @@
-import { Suspense } from "react";
-import { list } from "@vercel/blob";
-import Image from "next/image";
+"use client";
 
-async function Video({ prefix }: { prefix: string }) {
-  const { blobs } = await list({ prefix, limit: 1 });
-  const { url } = blobs[0];
+import { useState, useRef, useEffect } from 'react';
+import Image from 'next/image';
+
+export default function Background() {
+  const [isLoading, setIsLoading] = useState(true);
+  const videoRef = useRef<HTMLVideoElement>(null!);
+  const timeoutRef = useRef<ReturnType<typeof setTimeout>>();
+
+  useEffect(() => {
+    const videoElement = videoRef.current;
+    
+    function checkVideoReady() {
+      if (videoRef.current.readyState >= 2) { 
+        setIsLoading(false);
+      } else {
+        timeoutRef.current = setTimeout(() => {
+          setIsLoading(false);
+        }, 5000);
+      }
+    };
+
+    checkVideoReady();
+
+    videoElement.addEventListener('loadedmetadata', checkVideoReady);
+    videoElement.addEventListener('canplay', checkVideoReady);
+
+    return () => {
+      clearTimeout(timeoutRef.current)
+
+      videoElement.removeEventListener('loadedmetadata', checkVideoReady);
+      videoElement.removeEventListener('canplay', checkVideoReady);
+    };
+  }, []);
 
   return (
-    <video
-      className="fixed inset-0 w-full h-full object-cover"
-      preload="none"
-      autoPlay
-      muted
-      loop
-      playsInline
-      controls={false}
-    >
-      <source src={url} type="video/mp4" />
-    </video>
-  );
-}
-
-export default function Home() {
-  return (
-    <Suspense
-      fallback={
+    <div className="relative w-full h-full">
+      {isLoading && (
         <Image
           className="fixed inset-0 w-full h-full object-cover"
           src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAAECAIAAAArjXluAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAJElEQVR4nGNgY+WWV1Bh2Dp/s4SUHMO3L/9VdQ0Y1HWMfKJSAWKpB8xGxRZgAAAAAElFTkSuQmCC"
@@ -32,9 +44,20 @@ export default function Home() {
           width={1214}
           height={2160}
         />
-      }
-    >
-      <Video prefix="IMG_7170-nOqHfkiTopRM3JVs55jWyvku7qVuno.mp4" />
-    </Suspense>
+      )}
+      <video
+        className="fixed inset-0 w-full h-full object-cover"
+        autoPlay
+        controls={false}
+        loop
+        muted
+        playsInline
+        preload="auto"
+        ref={videoRef}
+        style={{ visibility: isLoading ? 'hidden' : 'visible' }}
+      >
+        <source src="/IMG_7170.webm" type="video/webm" />
+      </video>
+    </div>
   );
 }
